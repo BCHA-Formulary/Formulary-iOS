31a4b84e8f6273a3dbdc5ada2125435592cbfb02
Kelvin Chan <chan.kelv@gmail.com>
2016-07-14 22:38:25 -0700

    Added autocomplete to search field

--- BCHA-Formulary/BCHA-Formulary/MainViewController.swift
+++ BCHA-Formulary/BCHA-Formulary/MainViewController.swift
@@ -9,24 +9,31 @@
 import UIKit
 import Firebase
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, MPGTextFieldDelegate {
 
-    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var searchField: MPGTextField_Swift!
     @IBOutlet weak var searchBttn: UIButton!
     @IBOutlet weak var helpButton: UIButton!
     
     var firebase:FirebaseHelper!
+    var sql:SqlHelper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         firebase = FirebaseHelper.init()
-        if(!firebase.isUpToDate()){ //TODO this should be !isUpToDate, to save calls for now, set to opposite
+        if(firebase.isUpToDate()){ //TODO this should be !isUpToDate, to save calls for now, set to opposite
             print("Needs update")
             FirebaseHelper.updateFirebaseDrugList(Status.FORMULARY)
             FirebaseHelper.updateFirebaseDrugList(Status.EXCLUDED)
             FirebaseHelper.updateFirebaseDrugList(Status.RESTRICTED)
         }
+        sql = SqlHelper.init()
+        searchField.mDelegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +54,24 @@ class MainViewController: UIViewController {
     @IBAction func helpScreen(sender: AnyObject) {
 //        FirebaseHelper.init().isUpToDate()
         SqlHelper.init().rowCount()
+    }
+    
+    func dataForPopoverInTextField(textfield: MPGTextField_Swift) -> [Dictionary<String, AnyObject>]
+    {
+        var sampleData = [Dictionary<String, AnyObject>]()
+        let drugNames = sql.getAllDrugNames()
+        for name in drugNames{
+            let dictionary = ["DisplayText":name, "DisplaySubText":"", "CustomObject":""]
+            sampleData.append(dictionary)
+        }
+        return sampleData
+    }
+    func textFieldShouldSelect(textField: MPGTextField_Swift) -> Bool{
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: MPGTextField_Swift, withSelection data: Dictionary<String,AnyObject>){
+        print("Dictionary received = \(data)")
     }
 }
 
